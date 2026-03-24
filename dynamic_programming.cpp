355aec0321f371be663bd1b04bccbffc5dedd007
premmallik091-afk <premmallik091@gmail.com>
2026-03-24 11:00:19 +0530

    Implement Fibonacci and Knapsack algorithms
    Added Fibonacci and Knapsack functions using dynamic programming.

--- dynamic_programming.cpp
+++ dynamic_programming.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    int dp[100];
+    dp[0]=0; dp[1]=1;
+    for(int i=2;i<=n;i++)
+        dp[i]=dp[i-1]+dp[i-2];
+    return dp[n];
+}
+
+int knapsack() {
+    int wt[]={10,20,30}, val[]={60,100,120};
+    int W=50, n=3;
+    int dp[100][100]={0};
+
+    for(int i=1;i<=n;i++){
+        for(int w=1;w<=W;w++){
+            if(wt[i-1]<=w)
+                dp[i][w]=max(val[i-1]+dp[i-1][w-wt[i-1]], dp[i-1][w]);
+            else
+                dp[i][w]=dp[i-1][w];
+        }
+    }
+    return dp[n][W];
+}
+
+int main() {
+    cout<<"Fibonacci(10): "<<fib(10)<<endl;
+    cout<<"Knapsack: "<<knapsack()<<endl;
+    return 0;
+}
